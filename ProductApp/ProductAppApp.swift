cbdc16712afb790f29be9e3b0d2257d65c706b9b
Allison <suprun-s@bk.ru>
2024-01-31 23:22:26 +0300

    Initial Commit

---
+++ ProductApp/ProductAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAppApp.swift
+//  ProductApp
+//
+//  Created by Alexander Suprun on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
